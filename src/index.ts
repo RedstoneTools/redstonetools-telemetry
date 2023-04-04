cbfcdce7573c45cad14e504b10e49362dd1c89f4
rugmj <rugmj@rugmj.dev>
2023-04-04 08:47:29 +0000

    Adds session endpoints

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,22 @@ const app = express();
 
 const v1 = express.Router();
 
+app.use(express.json());
 app.use('/api/v1', v1);
 
-// /api/v1/crash
-// /api/v1/error
-// /api/v1/command
+interface MojangAuth {
+	uuid: String;
+	profile_id: String;
+	access_token: String;
+}
+
+v1.post('/session/create', (req, res) => {
+	const { uuid, profile_id, access_token }: MojangAuth = req.body;
+});
+
+v1.post('/session/refresh', (req, res) => {
+	const { uuid, profile_id, access_token }: MojangAuth = req.body;
+});
 
 v1.post('/crash', (req, res) => {
 	res.sendStatus(200);
