a5b9c4069d902ef778cf92cdbda579263dd24d30
rugmj <rugmj@rugmj.dev>
2023-04-06 13:06:02 +0000

    swapped database packages

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,19 @@
 import express, { NextFunction } from 'express';
-import postgres from 'postgres';
+import { Pool } from 'pg';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 import config from './config.json' assert { type: 'json' };
 
-const { AUTH_SERVER, JWT_SECRET, EXPIRE_TIME_SECS } = config;
+const { AUTH_SERVER, JWT_SECRET, EXPIRE_TIME_SECS, DB } = config;
 
-const sql = postgres(); // Uses environment variables for database connection (https://www.postgresql.org/docs/current/libpq-envars.html)
+const pool = new Pool(DB);
 
 const app = express();
 
 const v1 = express.Router();
 
 app.use(express.json());
-
-// app.use((req, res, next) => {
-// 	jwt.verify(req.body.token, JWT_SECRET);
-// });
 
 app.use('/api/v1', v1);
 
@@ -47,29 +43,54 @@ v1.post('/session/create', async (req, res) => {
 	res.send(token);
 });
 
-v1.post('/exception', verifyToken, (req, res) => {
+v1.post('/session/refresh', async (req, res) => {});
+
+v1.post('/exception', verifyTokenMiddleware, (req, res) => {
 	res.sendStatus(200);
 });
 
-v1.post('/command', verifyToken, (req, res) => {
+v1.post('/command', verifyTokenMiddleware, (req, res) => {
 	res.sendStatus(200);
 });
 
-function verifyToken(req, res, next: NextFunction) {
+function verifyTokenMiddleware(req, res, next: NextFunction) {
 	const token = req.body.token;
-	if (!token)
+	// if (!token)
+	// 	return res.status(403).send('Could not find token in request body');
+
+	// jwt.verify(token, JWT_SECRET, (err, decoded) => {
+	// 	if (err) {
+	// 		if (err.name === 'TokenExpiredError')
+	// 			return res.status(403).send(`Token expired at ${err.expiredAt}`);
+
+	// 		return res.status(403).send('Invalid Credentials');
+	// 	}
+
+	// 	req.body.hashedServerId = decoded.hashedServerId;
+	// 	next();
+	// });
+
+	const decoded = verifyToken(token);
+	if (decoded === 'NoToken')
 		return res.status(403).send('Could not find token in request body');
+}
 
-	jwt.verify(token, JWT_SECRET, (err, decoded) => {
+function verifyToken(token, allowExpired = false) {
+	if (!token) return 'NoToken';
+
+	let output: string | jwt.JwtPayload;
+
+	jwt.verify(token, JWT_SECRET, (err, decoded: string | jwt.JwtPayload) => {
 		if (err) {
-			if (err.name === 'TokenExpiredError')
-				return res.status(403).send(`Token expired at ${err.expiredAt}`);
-
-			return res.status(403).send('Invalid Credentials');
+			if (err.name === 'TokenExpiredError' && !allowExpired)
+				return 'TokenExpired';
+			return 'InvalidToken';
 		}
 
-		req.body.hashedServerId = decoded.hashedServerId;
-		next();
+		output = decoded;
 	});
+
+	return output;
 }
+
 app.listen(3000);
