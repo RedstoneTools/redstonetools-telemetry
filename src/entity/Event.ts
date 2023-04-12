ad810f3aafcb85853fcff5b5302c9217bc3be946
Matthias Wijnsma <matthiasx95@gmail.com>
2023-04-12 23:28:43 +0200

    Remove entity annotation so typeorm doesnt create a table for the abstract event class

--- src/entity/Event.ts
+++ src/entity/Event.ts
@@ -2,12 +2,10 @@ import { Session } from './Session.js';
 
 import {
 	Column,
-	Entity,
 	ManyToOne,
 	PrimaryGeneratedColumn,
 } from 'typeorm';
 
-@Entity()
 export abstract class Event {
 	@PrimaryGeneratedColumn()
 	id: number;
