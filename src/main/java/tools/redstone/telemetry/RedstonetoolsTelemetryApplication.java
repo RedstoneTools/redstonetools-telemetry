8d14d1f6abbc5a248708ae94b084b737e61e8024
Matthias Wijnsma <matthiasx95@gmail.com>
2023-05-13 00:21:01 +0200

    Initial commit

---
+++ src/main/java/tools/redstone/telemetry/RedstonetoolsTelemetryApplication.java
@@ -0,0 +1,13 @@
+package tools.redstone.telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedstonetoolsTelemetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedstonetoolsTelemetryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tools/redstone/telemetry/RedstonetoolsTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package tools.redstone.telemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedstonetoolsTelemetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
